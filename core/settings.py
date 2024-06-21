5efb1ddfc5842429d80905cb93a94c48b10da439
MOHAN NEPALI <nepalimohan@outlook.com>
2024-06-21 17:24:29 +0545

    postgres try but failed

--- core/settings.py
+++ core/settings.py
@@ -65,13 +65,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
-
 
 # settings.py
 ELASTICSEARCH_DSL = {
