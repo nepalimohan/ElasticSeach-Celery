e0c570050f769711a45ed36f0324ea54c172e6f3
MOHAN NEPALI <nepalimohan@outlook.com>
2024-06-21 11:16:47 +0545

    urls in product and commands setup

---
+++ product/management/commands/create_products.py
@@ -0,0 +1,22 @@
+import faker
+from django.core.management.base import BaseCommand
+from product.models import Product
+
+
+class Command(BaseCommand):
+    help = 'Insert 100 books into the database'
+
+    def handle(self, *args, **kwargs):
+        fake = faker.Faker()
+
+        books = []
+        for _ in range(10000):
+            book = Product(
+                name=fake.sentence(nb_words=5),
+                category=fake.name(),
+                description=fake.text()
+            )
+            books.append(book)
+        
+        Product.objects.bulk_create(books)
+        self.stdout.write(self.style.SUCCESS('Successfully inserted 100000 books'))
--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ['-id']
+        verbose_name_plural = 'Products'
+        
+    
+    def __str__(self):
+        return f"{self.name} | {self.category}"
+    
